0ccccf063c2471c41b7e28ec24aa9ab129d6cdc3
microna <kievonfire@gmail.com>
2025-06-24 12:37:41 +0300

    added advantages sections

--- theme/index.php
+++ theme/index.php
@@ -224,6 +224,53 @@
     ?>
 
 
+    <?php
+    // Debug: Check for flexible content fields
+    $flexible_fields = ['advatages_sections']; // Fixed: Use your actual field name
+    $active_field = null;
+
+    foreach ($flexible_fields as $field) {
+        if (have_rows($field)) {
+            $active_field = $field;
+            break;
+        }
+    }
+     // Debug: Output field information
+     echo '<!-- Debug: Content block field: ' . ($active_field ? $active_field : 'none') . ' -->';
+
+     if ($active_field && have_rows($active_field)) {
+         while (have_rows($active_field)) {
+             the_row();
+             $layout = get_row_layout();    
+             echo '<!-- Debug: Content layout: ' . $layout . ' -->';
+             
+             // Switch case based on layout type
+             switch ($layout) {
+                 case 'advantages_1': // This might be the actual layout name  
+                     set_query_var('section_data', get_row());
+                     get_template_part('template-parts/flexible-content/advantages/advantages-1');
+                     break;
+                 case 'advantages_2': // This might be the actual layout name  
+                     set_query_var('section_data', get_row());
+                     get_template_part('template-parts/flexible-content/advantages/advantages-2');
+                     break;
+                 case 'advantages_3': // This might be the actual layout name  
+                     set_query_var('section_data', get_row());
+                     get_template_part('template-parts/flexible-content/advantages/advantages-3');
+                     break;
+               
+ 
+                 default:
+                     echo '<p>Unknown layout: ' . $layout . '</p>';
+                     break;
+             }
+         }
+     } else {
+         echo '<p>This page is empty</p>';
+     }
+    ?>
+
+
 </div>
 
 <?php get_footer(); ?>
---
+++ theme/template-parts/flexible-content/advantages/advantages-1.php
@@ -0,0 +1,39 @@
+<?php
+$image = get_sub_field('image');
+?>
+<div class="advantages">
+    <div class="container">
+        <div class="advantages__wrapper">
+            <h2 class="header-xl advantages__title">Title - main idea of the text</h2>
+            <div class="advantages__inner">
+                <div class="advantages__item">
+                    <img class="advantages__item-image" src="<?php echo $image; ?>" alt="" />
+                    <h3 class="header-m advantages__item-title">
+                        Title - main idea of the text
+                    </h3>
+                    <p class="text-m advantages__item-description">
+                        Description - short description of the text
+                    </p>
+                </div>
+                <div class="advantages__item">
+                    <img class="advantages__item-image" src="<?php echo $image; ?>" alt="" />
+                    <h3 class="header-m advantages__item-title">
+                        Title - main idea of the text
+                    </h3>
+                    <p class="text-m advantages__item-description">
+                        Description - short description of the text
+                    </p>
+                </div>
+                <div class="advantages__item">
+                    <img class="advantages__item-image" src="<?php echo $image; ?>" alt="" />
+                    <h3 class="header-m advantages__item-title">
+                        Title - main idea of the text
+                    </h3>
+                    <p class="text-m advantages__item-description">
+                        Description - short description of the text
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
